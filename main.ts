f4e5a80c29dd1fee1a7fca762740faab78544669
Bernhard <61845524+bernhardle@users.noreply.github.com>
2023-02-26 19:20:48 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,11 +9,17 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     basic.setLedColor(0x000000)
 })
+serial.redirect(
+SerialPin.C17,
+SerialPin.C16,
+BaudRate.BaudRate115200
+)
 let counter = 0
 pins.setPull(DigitalPin.P3, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 basic.forever(function () {
     basic.pause(1000)
     basic.showNumber(counter)
+    serial.writeValue("counts", counter)
     counter = 0
 })

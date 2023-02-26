a694811d009ea596e4d140ef58d8768d522669f3
Bernhard <61845524+bernhardle@users.noreply.github.com>
2023-02-26 17:41:22 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.setLedColor(0x0000ff)
 })
 pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
     counter += 1
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.setLedColor(0x000000)
 })
 let counter = 0
 pins.setPull(DigitalPin.P3, PinPullMode.PullUp)

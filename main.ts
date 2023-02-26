1198719956721f045d327f4489abe37599410083
Bernhard <61845524+bernhardle@users.noreply.github.com>
2023-02-26 19:48:08 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -17,9 +17,13 @@ BaudRate.BaudRate115200
 let counter = 0
 pins.setPull(DigitalPin.P3, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
+pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
 basic.forever(function () {
     basic.pause(1000)
     basic.showNumber(counter)
     serial.writeValue("counts", counter)
+    serial.writeValue("P1", pins.analogReadPin(AnalogPin.P1))
+    serial.writeValue("P2", pins.analogReadPin(AnalogPin.P2))
     counter = 0
 })

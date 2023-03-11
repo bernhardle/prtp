3f947888b429f3b51d93d5462fe502970867b6ff
Bernhard <61845524+bernhardle@users.noreply.github.com>
2023-03-11 10:35:23 +0100

    Skalierung der Ausgabe entsprechend den ersten Vergleichen mit ROPULS

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     pins.digitalWritePin(DigitalPin.P0, 1)
+    music.playTone(988, music.beat(BeatFraction.Eighth))
     basic.setLedColor(0x0000ff)
 })
 pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
@@ -7,6 +8,7 @@ pins.onPulsed(DigitalPin.P3, PulseValue.Low, function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
+    music.playTone(131, music.beat(BeatFraction.Eighth))
     basic.setLedColor(0x000000)
 })
 serial.redirectToUSB()
@@ -16,10 +18,11 @@ pins.setPull(DigitalPin.P3, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
+pins.digitalWritePin(DigitalPin.P0, 0)
 serial.writeLine("Initialized.")
 basic.forever(function () {
     basic.pause(1000)
-    basic.showNumber(counter)
+    basic.showNumber(Math.round(counter * 37 / 49))
     serial.writeValue("counts", counter)
     serial.writeValue("P1", pins.analogReadPin(AnalogPin.P1))
     serial.writeValue("P2", pins.analogReadPin(AnalogPin.P2))

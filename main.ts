cf92abce8ab55035cc7e4aa089a4cbd7c21118a3
Bernhard <61845524+bernhardle@users.noreply.github.com>
2023-03-17 19:47:14 +0100

    First pulse flash after relay off

--- main.ts
+++ main.ts
@@ -1,19 +1,28 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    music.playTone(988, music.beat(BeatFraction.Eighth))
-    basic.setLedColor(0x0000ff)
-})
 pins.onPulsed(DigitalPin.P3, PulseValue.High, function () {
-    counter += 1
+    pulsecounter += 1
+    tmp = diverseTools.timeStamp()
+    serial.writeValue("Time", tmp - timestamp)
+    timestamp = tmp
+    if (pulseoff) {
+        pulseoff = false
+        blinklatch = true
+    }
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    music.playTone(131, music.beat(BeatFraction.Eighth))
-    basic.setLedColor(0x000000)
-})
-let counter = 0
+let tmp = 0
+let pulsecounter = 0
+let blinklatch = false
+let pulseoff = false
+let timestamp = 0
 serial.redirectToUSB()
 serial.setBaudRate(BaudRate.BaudRate115200)
+let relaystate = false
+timestamp = diverseTools.timeStamp()
+pulseoff = false
+blinklatch = false
+let ledrelayon = basic.rgb(0, 0, 255)
+let ledrelayoff = basic.rgb(0, 0, 0)
+let ledflashfirst = basic.rgb(255, 255, 255)
+let ledrelay = ledrelayoff
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
@@ -22,9 +31,37 @@ pins.digitalWritePin(DigitalPin.P0, 0)
 serial.writeLine("Initialized.")
 basic.forever(function () {
     basic.pause(1000)
-    basic.showNumber(Math.round(counter * 37 / 49))
-    serial.writeValue("counts", counter)
+    basic.showNumber(Math.round(pulsecounter * 37 / 49))
+    serial.writeValue("counts", pulsecounter)
     serial.writeValue("P1", pins.analogReadPin(AnalogPin.P1))
     serial.writeValue("P2", pins.analogReadPin(AnalogPin.P2))
-    counter = 0
+    pulsecounter = 0
+})
+control.inBackground(function () {
+    while (true) {
+        if (input.buttonIsPressed(Button.A)) {
+            if (!(relaystate)) {
+                music.playTone(988, music.beat(BeatFraction.Eighth))
+                pins.digitalWritePin(DigitalPin.P0, 1)
+                relaystate = true
+                ledrelay = ledrelayon
+                pulseoff = false
+            }
+        } else {
+            if (relaystate) {
+                music.playTone(131, music.beat(BeatFraction.Eighth))
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                relaystate = false
+                ledrelay = ledrelayoff
+                pulseoff = true
+            }
+        }
+        if (blinklatch) {
+            blinklatch = false
+            basic.setLedColor(ledflashfirst)
+        }
+        basic.pause(20)
+        basic.setLedColor(ledrelay)
+        basic.pause(80)
+    }
 })

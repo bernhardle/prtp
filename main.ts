8bd8782e25f13273c13fa85e83a412bfe46878be
Bernhard <61845524+bernhardle@users.noreply.github.com>
2023-03-21 21:11:13 +0100

    End of day commit.

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     if (!(autopulse)) {
-        music.playTone(131, music.beat(BeatFraction.Eighth))
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        ledrelay = ledrelayoff
-        pulseoff = true
+        stoppulse()
     }
 })
+function stoppulse () {
+    if (0 != relaystate) {
+        pulseoff = true
+        relaystate = 0
+        pins.digitalWritePin(DigitalPin.P0, relaystate)
+        music.playTone(131, music.beat(BeatFraction.Eighth))
+    }
+    ledrelay = ledrelayoff
+    basic.setLedColor(ledrelay)
+}
+function startpulse () {
+    if (1 != relaystate) {
+        pulseoff = false
+        relaystate = 1
+        pins.digitalWritePin(DigitalPin.P0, relaystate)
+        music.playTone(988, music.beat(BeatFraction.Eighth))
+    }
+    ledrelay = ledrelayon
+    basic.setLedColor(ledrelay)
+}
 control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     if (!(autopulse)) {
-        music.playTone(988, music.beat(BeatFraction.Eighth))
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        ledrelay = ledrelayon
-        pulseoff = false
+        startpulse()
     }
 })
 pins.onPulsed(DigitalPin.P3, PulseValue.High, function () {
@@ -28,19 +42,15 @@ pins.onPulsed(DigitalPin.P3, PulseValue.High, function () {
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     if (autopulse) {
         autopulse = false
-        pulseoff = true
-        pins.digitalWritePin(DigitalPin.P0, 0)
         ledrelayoff = basic.rgb(0, 0, 0)
-        ledrelay = ledrelayoff
+        stoppulse()
     } else {
         autopulse = true
-        pulseoff = false
-        ledrelayoff = basic.rgb(0, 32, 12)
-        ledrelay = ledrelayon
+        ledrelayoff = basic.rgb(0, 0, 32)
     }
-    basic.setLedColor(ledrelay)
 })
 let tmp = 0
+let relaystate = 0
 let autopulse = false
 let ledrelay = 0
 let ledrelayoff = 0
@@ -54,43 +64,38 @@ timestamp = diverseTools.timeStamp()
 // Variable is 'true' after relay has been switched to 'off' until next flow meter pulse is received.
 pulseoff = false
 blinklatch = false
-ledrelayon = basic.rgb(0, 0, 128)
+ledrelayon = basic.rgb(0, 0, 164)
 ledrelayoff = basic.rgb(0, 0, 0)
 let ledflashfirst = basic.rgb(255, 255, 255)
 ledrelay = ledrelayoff
 autopulse = false
+relaystate = 0
 let swirlcount = 0
 let swirlcountlap = 0
 pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P3, PinPullMode.PullNone)
-pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P0, relaystate)
 led.setBrightness(128)
-serial.writeLine("Initialized.")
-basic.forever(function () {
-    basic.pause(1000)
+serial.writeLine("" + control.deviceName() + " initialized.")
+basic.showString(control.deviceName())
+loops.everyInterval(1000, function () {
     basic.showNumber(Math.round(swirlcountlap * 37 / 49))
-    serial.writeLine("" + convertToText(swirlcount) + " " + convertToText(swirlcountlap) + " " + convertToText(pins.analogReadPin(AnalogPin.P1)) + " " + convertToText(pins.analogReadPin(AnalogPin.P2)) + " " + convertToText(input.soundLevel()) + " " + convertToText(pins.digitalReadPin(DigitalPin.P0)))
     swirlcountlap = 0
+    serial.writeLine("" + convertToText(swirlcount) + " " + convertToText(swirlcountlap) + " " + convertToText(pins.analogReadPin(AnalogPin.P1)) + " " + convertToText(pins.analogReadPin(AnalogPin.P2)) + " " + convertToText(input.soundLevel()) + " " + convertToText(relaystate))
 })
 control.inBackground(function () {
     while (true) {
         if (autopulse) {
-            pins.digitalWritePin(DigitalPin.P0, 1)
-            ledrelay = ledrelayon
-            pulseoff = false
-            basic.setLedColor(ledrelay)
+            startpulse()
             for (let index = 0; index < 25; index++) {
                 if (autopulse) {
                     basic.pause(200)
                 }
             }
             if (autopulse) {
-                pins.digitalWritePin(DigitalPin.P0, 0)
-                ledrelay = ledrelayoff
-                pulseoff = true
-                basic.setLedColor(ledrelay)
+                stoppulse()
                 for (let index = 0; index < 10; index++) {
                     if (autopulse) {
                         basic.pause(200)
